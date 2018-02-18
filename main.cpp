faba7faee1208d4859abb15ab38e5ee824d58161
evgenykol <edkolotilov@gmail.com>
2018-02-18 22:51:38 +0300

    Слегка доработал. Сделал вывод в файл. Нужно навести порядок в зависимостях

--- main.cpp
+++ main.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <ctime>
 
-
-#define LOCAL_DEBUG 1
+//#define LOCAL_DEBUG 1
 using namespace std;
 
-using Commands = vector<string>;
+class Commands
+{
+public:
+    vector<string> cmds;
+    time_t timestamp;
+    int cmdCounter;
+
+    void push_back(string str)
+    {
+        if(!cmds.size())
+        {
+            timestamp = time(nullptr);
+        }
+        cmds.push_back(str);
+        ++cmdCounter;
+    }
+
+    void push_back_block(string str)
+    {
+        if(!cmds.size())
+        {
+            timestamp = time(nullptr);
+        }
+        cmds.push_back(str);
+    }
+
+    void clear()
+    {
+        cmds.clear();
+        cmdCounter = 0;
+    }
+};
 
 class Observer
 {
@@ -32,7 +64,7 @@ public:
         }
     }
 
-    void dump_commands(Commands cmd)
+    void dump_commands(Commands &cmd)
     {
         cmds = cmd;
         notify();
@@ -51,7 +83,7 @@ public:
     {
         bool is_first = true;
         cout << "bulk: ";
-        for(auto s : cmd)
+        for(auto s : cmd.cmds)
         {
             if(is_first)
             {
@@ -77,102 +109,123 @@ public:
 
     void dump(Commands &cmd)
     {
-        cout << "dump from FileDumper" << endl;
+        string filename = "bulk" + to_string(cmd.timestamp) + ".log";
+        ofstream of(filename);
+
+        bool is_first = true;
+        of << "bulk: ";
+        for(auto s : cmd.cmds)
+        {
+            if(is_first)
+            {
+                is_first = false;
+            }
+            else
+            {
+                of << ", ";
+            }
+            of << s;
+        }
+        of << endl;
+        of.close();
     }
 };
 
-class Processor
-{
-    Commands cmds;
-    void addLine(string line);
-};
+
 
 int main(int argc, char *argv[])
 {
+    try
+    {
+
 #ifdef LOCAL_DEBUG
     freopen("cmd_input.txt", "rt", stdin);
 #endif
 
-    int commandsCount = 0;
-    if ((argc > 1) &&
-       (!strncmp(argv[1], "-v", 2) || !strncmp(argv[1], "--version", 9)))
-    {
-        cout << "version " /*<< version()*/ << endl;
-        return 0;
-    }
-    else if (argc > 1) {
-        commandsCount = atoi(argv[1]);
-    }
-    else
-    {
-        cout << "bad input" << endl;
-        return 0;
-    }
-
-    Dumper dumper;
-
-    ConsoleDumper conDumper(&dumper);
-    FileDumper fileDumper(&dumper);
-
-    vector<string> cmds;
-    string line;
-    int curCounter = 0;
-    bool blockFound = false;
-    int nestedBlocksCount = 0;
-    while(getline(cin, line))
-    {
-        if((line != "{") && !blockFound)
+        int commandsCount = 0;
+        if ((argc > 1) &&
+           (!strncmp(argv[1], "-v", 2) || !strncmp(argv[1], "--version", 9)))
         {
-            ++curCounter;
-            cmds.push_back(line);
-
-            if(curCounter == commandsCount)
-            {
-                dumper.dump_commands(cmds);
-                curCounter = 0;
-                cmds.clear();
-            }
+            cout << "version " /*<< version()*/ << endl;
+            return 0;
+        }
+        else if (argc > 1) {
+            commandsCount = atoi(argv[1]);
         }
         else
         {
-            if(!blockFound)
-            {
-                blockFound = true;
-                if(cmds.size())
-                {
-                    dumper.dump_commands(cmds);
-                    cmds.clear();
-                }
-                continue;
-            }
+            cout << "bad input" << endl;
+            return 0;
+        }
 
-            if(line == "{")
+        Dumper dumper;
+
+        ConsoleDumper conDumper(&dumper);
+        FileDumper fileDumper(&dumper);
+
+        Commands cmds;
+        string line;
+        int curCounter = 0;
+        bool blockFound = false;
+        int nestedBlocksCount = 0;
+
+        while(getline(cin, line))
+        {
+            if((line != "{") && !blockFound)
             {
-                ++nestedBlocksCount;
-            }
-            else if(line == "}")
-            {
-                if (nestedBlocksCount > 0)
-                {
-                    --nestedBlocksCount;
-                }
-                else
+                cmds.push_back(line);
+
+                if(cmds.cmdCounter == commandsCount)
                 {
                     dumper.dump_commands(cmds);
                     cmds.clear();
-                    blockFound = false;
                 }
             }
             else
             {
-                cmds.push_back(line);
+                if(!blockFound)
+                {
+                    blockFound = true;
+                    if(cmds.cmdCounter)
+                    {
+                        dumper.dump_commands(cmds);
+                        cmds.clear();
+                    }
+                    continue;
+                }
+
+                if(line == "{")
+                {
+                    ++nestedBlocksCount;
+                }
+                else if(line == "}")
+                {
+                    if (nestedBlocksCount > 0)
+                    {
+                        --nestedBlocksCount;
+                    }
+                    else
+                    {
+                        dumper.dump_commands(cmds);
+                        cmds.clear();
+                        blockFound = false;
+                    }
+                }
+                else
+                {
+                    cmds.push_back_block(line);
+                }
             }
         }
-    }
 
-    if(curCounter)
+        if(cmds.cmdCounter)
+        {
+            dumper.dump_commands(cmds);
+        }
+    }
+    catch(exception &e)
     {
-        dumper.dump_commands(cmds);
+        cout << e.what();
     }
     return 0;
 }

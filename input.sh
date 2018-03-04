2962cfc12750d6b8bd7ff9e54dd826f57d56e7a2
evgenykol <edkolotilov@gmail.com>
2018-03-04 21:38:56 +0300

    Некорректно работает на тестовой машине. Попробую перенести реализацию из Д3 11.

--- input.sh
+++ input.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-arr=("cmd33" "cmd2" "cmd3" "cmd4" "cmd5" "cmd6" "cmd7")
+#arr=("cmd1" "cmd2" "cmd3" "cmd4" "cmd5")
+#arr=("cmd1" "cmd2" "cmd3" "{" "cmd4" "cmd5" "cmd6" "cmd7" "}")
+#arr=("{" "cmd1" "cmd2" "{" "cmd3" "cmd4" "}" "cmd5" "cmd6" "}")
+#arr=("cmd1" "cmd2" "cmd3" "{" "cmd4" "cmd5" "cmd6" "cmd7")
+arr=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
+
 for (( i = 0; i < ${#arr[@]}; i++ )); do
 	echo ${arr[$i]}
-	sleep 1
+	#sleep 0.1
 done
--- main.cpp
+++ main.cpp
@@ -1,150 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <ctime>
-#include "string.h"
 
 #include "version.h"
+#include "bulk.h"
 
-//#define LOCAL_DEBUG 1
 using namespace std;
-
-class Commands
-{
-public:
-    vector<string> cmds;
-    time_t timestamp;
-    int cmdCounter;
-
-    void push_back(string str)
-    {
-        if(!cmds.size())
-        {
-            timestamp = time(nullptr);
-        }
-        cmds.push_back(str);
-        ++cmdCounter;
-    }
-
-    void push_back_block(string str)
-    {
-        if(!cmds.size())
-        {
-            timestamp = time(nullptr);
-        }
-        cmds.push_back(str);
-    }
-
-    void clear()
-    {
-        cmds.clear();
-        cmdCounter = 0;
-    }
-};
-
-class Observer
-{
-public:
-    virtual void dump(Commands &cmd) = 0;
-};
-
-class Dumper
-{
-    Commands cmds;
-    vector<Observer *> subs;
-public:
-    void subscribe(Observer *ob)
-    {
-        subs.push_back(ob);
-    }
-
-    void notify()
-    {
-        for (auto s : subs)
-        {
-            s->dump(cmds);
-        }
-    }
-
-    void dump_commands(Commands &cmd)
-    {
-        cmds = cmd;
-        notify();
-    }
-};
-
-class ConsoleDumper : public Observer
-{
-public:
-    ConsoleDumper(Dumper *dmp)
-    {
-        dmp->subscribe(this);
-    }
-
-    void dump(Commands &cmd)
-    {
-        bool is_first = true;
-        cout << "bulk: ";
-        for(auto s : cmd.cmds)
-        {
-            if(is_first)
-            {
-                is_first = false;
-            }
-            else
-            {
-                cout << ", ";
-            }
-            cout << s;
-        }
-        cout << endl;
-    }
-};
-
-class FileDumper : public Observer
-{
-public:
-    FileDumper(Dumper *dmp)
-    {
-        dmp->subscribe(this);
-    }
-
-    void dump(Commands &cmd)
-    {
-        string filename = "bulk" + to_string(cmd.timestamp) + ".log";
-        ofstream of(filename);
-
-        bool is_first = true;
-        of << "bulk: ";
-        for(auto s : cmd.cmds)
-        {
-            if(is_first)
-            {
-                is_first = false;
-            }
-            else
-            {
-                of << ", ";
-            }
-            of << s;
-        }
-        of << endl;
-        of.close();
-    }
-};
-
-
 
 int main(int argc, char *argv[])
 {
     try
     {
-
-#ifdef LOCAL_DEBUG
-    freopen("cmd_input.txt", "rt", stdin);
-#endif
-
         int commandsCount = 0;
         if ((argc > 1) &&
            (!strncmp(argv[1], "-v", 2) || !strncmp(argv[1], "--version", 9)))
@@ -161,70 +25,13 @@ int main(int argc, char *argv[])
             return 0;
         }
 
-        Dumper dumper;
-
-        ConsoleDumper conDumper(&dumper);
-        FileDumper fileDumper(&dumper);
-
-        Commands cmds;
+        auto blk = bulk::BulkContext(commandsCount);
         string line;
-        int curCounter = 0;
-        bool blockFound = false;
-        int nestedBlocksCount = 0;
-
         while(getline(cin, line))
         {
-            if((line != "{") && !blockFound)
-            {
-                cmds.push_back(line);
-
-                if(cmds.cmdCounter == commandsCount)
-                {
-                    dumper.dump_commands(cmds);
-                    cmds.clear();
-                }
-            }
-            else
-            {
-                if(!blockFound)
-                {
-                    blockFound = true;
-                    if(cmds.cmdCounter)
-                    {
-                        dumper.dump_commands(cmds);
-                        cmds.clear();
-                    }
-                    continue;
-                }
-
-                if(line == "{")
-                {
-                    ++nestedBlocksCount;
-                }
-                else if(line == "}")
-                {
-                    if (nestedBlocksCount > 0)
-                    {
-                        --nestedBlocksCount;
-                    }
-                    else
-                    {
-                        dumper.dump_commands(cmds);
-                        cmds.clear();
-                        blockFound = false;
-                    }
-                }
-                else
-                {
-                    cmds.push_back_block(line);
-                }
-            }
+            blk.add_command(line);
         }
-
-        if(cmds.cmdCounter)
-        {
-            dumper.dump_commands(cmds);
-        }
+        blk.end_input();
     }
     catch(exception &e)
     {
